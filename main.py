4bf029a9051fb8cf0d522418c278e0205ce597e9
tranbdoan <tbdoan@andrew.cmu.edu>
2026-04-19 16:28:17 -0400

    added fruit movement/elimination and added trail to pointer

--- main.py
+++ main.py
@@ -7,17 +7,29 @@ from mediapipe.tasks.python import vision
 import time
 
 class Fruit:
-    def __init__(self, name, image,x, y, width, height,rotate=0):
+    def __init__(self, name, image,x, y, width, height):
         self.name = name
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.image = image
-        self.rotate = rotate
-    
+        self.rotate = random.randint(-30, 30)
+        self.dx = random.randint(-4,4)
+        self.dy = random.choice(list(range(-22, -10, 2)))
+        self.gravity = 0.5  
+        self.sliced = False
+
     def draw(self):
-        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, rotateAngle=self.rotate)
+        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, rotateAngle=self.rotate, align='center')
+    
+    def updatePosition(self):
+        self.x += self.dx
+        self.y += self.dy
+        self.dy += self.gravity
+    
+    def isLegal(self,app):
+        return self.height<app.height+100 and not self.sliced
 
 def onAppStart(app):
     # CV AND MEDIAPIPE SETUP
@@ -29,12 +41,15 @@ def onAppStart(app):
     # GAME STATE VARIABLES
     app.stepsPerSecond = 60
     app.handX, app.handY = 475, 260
+    app.targetHandCoords = [475,260]
+    app.trail = []
     app.score = 0   
     app.highScore = 0
     app.gameStarted = False
     app.showInstructions = False
     app.gameOver = False
     app.soundIsPlaying = True
+    app.livesLeft = 3
 
     # BG AND UI
     app.beach = 'FULL BG.png'
@@ -42,17 +57,11 @@ def onAppStart(app):
     app.sound = Sound('Beach Song.mp3')
 
     # FRUITS AND DECOR
-    app.fruits = [
-        Fruit('pineapple', 'Pineapple.png', 200, 50, 100, 170),
-        Fruit('dragonfruit', 'Dragonfruit.png', 40, 300, 80, 80),
-        Fruit('kiwi', 'Kiwi.png', 550, 200, 70, 70),
-        Fruit('coconut', 'Coconut.png', 400, 300, 60, 60),
-        Fruit('orange', 'Orange.png', 500, 400, 70, 70),
-        Fruit('mango', 'Mango.png', 250, 400, 60, 80),
-        Fruit('banana', 'Banana.png', 350, 200, 80, 80),
-        Fruit('flower', 'Flower.png', 425, 140, 90, 90),
-        Fruit('torch', 'Torch.png', 780, 150, 150, 150, rotate=-45)
-    ]
+    app.fruits = []
+
+    app.width = 950
+    app.height = 535
+    app.spawnRate = 0.07
 
 def setupHandTracker(app):
     model_path = 'hand_landmarker.task'
@@ -60,10 +69,8 @@ def setupHandTracker(app):
     def update_hand_position(result, output_image, timestamp_ms):
         if result.hand_landmarks:
             tip = result.hand_landmarks[0][8]
-            targetX = tip.x * app.width
-            targetY = tip.y * app.height
-            app.handX += (targetX - app.handX) * 0.5
-            app.handY += (targetY - app.handY) * 0.5
+            app.targetHandCoords[0] = tip.x * app.width
+            app.targetHandCoords[1] = tip.y * app.height
     options = vision.HandLandmarkerOptions(
         base_options=base_options,
         running_mode=vision.RunningMode.LIVE_STREAM,
@@ -71,7 +78,7 @@ def setupHandTracker(app):
         min_hand_detection_confidence=0.5,
         min_tracking_confidence=0.5,
         result_callback=update_hand_position
-    )
+        )
     app.detector = vision.HandLandmarker.create_from_options(options)
 
 def onMousePress(app,mouseX,mouseY):
@@ -90,6 +97,12 @@ def inBounds(mouseX,mouseY,x,y,width,height):
     return (x<=mouseX<=x+width) and (y<=mouseY<=y+height)
 
 def onStep(app):
+    app.handX += (app.targetHandCoords[0] - app.handX) * 0.5
+    app.handY += (app.targetHandCoords[1] - app.handY) * 0.5
+    app.trail.append((app.handX, app.handY))
+    if len(app.trail) > 10:
+        app.trail.pop(0)
+    # lines 103-109 used AI
     success, frame = app.cap.read()
     if success:
         frame = cv2.flip(frame, 1)
@@ -98,12 +111,55 @@ def onStep(app):
         timestamp_ms = int(time.time() * 1000)
         app.detector.detect_async(mp_image, timestamp_ms)
 
+    if app.gameStarted and not app.showInstructions and not app.gameOver:
+        if random.random() < app.spawnRate:
+            loadNextFruit(app)
+        for fruit in app.fruits:
+            fruit.updatePosition()
+            if distance(app.handX, app.handY, fruit.x, fruit.y) < max(fruit.width, fruit.height)/2:
+                if not fruit.sliced:
+                    app.score += 10
+                    fruit.sliced = True
+                if app.score > app.highScore:
+                    app.highScore = app.score
+        app.fruits = [fruit for fruit in app.fruits if fruit.isLegal(app)]
+
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
+
+def loadNextFruit(app):
+    fruits = {'pineapple': ('Pineapple.png',100, 170), 
+              'dragonfruit': ('Dragonfruit.png', 80, 80), 
+              'kiwi': ('Kiwi.png', 70, 70),
+              'coconut': ('Coconut.png', 60, 60),
+              'orange': ('Orange.png', 70, 70),
+              'mango': ('Mango.png', 60, 80),
+              'banana': ('Banana.png',80, 80),
+              'flower': ('Flower.png', 90, 90),
+              'torch': ('Torch.png', 150, 150)}
+    name = random.choice(list(fruits.keys()))
+    image, width, height = fruits[name]
+    x0 = random.randint(100, 800)
+    y0 = app.height 
+    newFruit = Fruit(name, image, x0, y0, width, height)
+    app.fruits.append(newFruit)
+
 def drawButtons(app):
     drawRect(830,10,100,35,fill='burlyWood',border='lemonChiffon',borderWidth=2)
-    drawLabel('SOUND',880,26,font='caveat',fill='lemonChiffon',size=22,border='lemonChiffon',borderWidth=1)
+    drawLabel('SOUND',880,26,font='caveat',fill='lemonChiffon',
+              size=22,border='lemonChiffon',borderWidth=1)
     if app.gameStarted or app.showInstructions:
         drawRect(20,10,100,35,fill='burlyWood',border='lemonChiffon',borderWidth=2)
-        drawLabel('HOME',70,26,font='caveat',fill='lemonChiffon',size=22,border='lemonChiffon',borderWidth=1)
+        drawLabel('HOME',70,26,font='caveat',fill='lemonChiffon',
+                  size=22,border='lemonChiffon',borderWidth=1)
+
+def drawTrail(app):
+    for i in range(len(app.trail)-1):
+        opacityFactor = i / len(app.trail)
+        opacity = int(100 * (opacityFactor))
+        drawCircle(app.trail[i][0], app.trail[i][1], 5, 
+                 fill=gradient('yellow', 'orange','fuchsia'), 
+                 opacity=opacity, border='pink', borderWidth=2)
 
 def redrawAll(app):
     if app.soundIsPlaying:
@@ -114,19 +170,22 @@ def redrawAll(app):
         drawImage(app.beach,0,0,width=950,height=535)
         if app.showInstructions:
             drawImage(app.playBG, 0, 0, width=app.width, height=app.height)
-            drawLabel('HOW TO PLAY:',470,95,font='caveat',size = 70,fill='saddleBrown',border='saddleBrown',borderWidth=1)
+            drawLabel('HOW TO PLAY:',470,95,font='caveat',size = 70,fill='saddleBrown',
+                      border='saddleBrown',borderWidth=1)
     else:
         drawImage(app.playBG, 0, 0, width=app.width, height=app.height)
         for fruit in app.fruits:
             fruit.draw()
 
-        drawCircle(app.handX, app.handY, 8, fill=gradient('mediumVioletRed', 'fuchsia'),border='pink',borderWidth=1)
-
         drawRect(250,10,150,35,fill='burlyWood',border='lemonChiffon',borderWidth=2)
-        drawLabel(f'HIGH: {app.highScore}',295,26,font='caveat',fill='lemonChiffon',size=22,border='lemonChiffon',borderWidth=1,align='center')
+        drawLabel(f'HIGH: {app.highScore}',295,26,font='caveat',fill='lemonChiffon',
+                  size=22,border='lemonChiffon',borderWidth=1,align='center')
         drawRect(550,10,150,35,fill='burlyWood',border='lemonChiffon',borderWidth=2)
-        drawLabel(f'{app.score}',625,26,font='caveat',fill='lemonChiffon',size=22,border='lemonChiffon',borderWidth=1,align='center')
-        drawCircle(app.handX, app.handY, 8, fill=gradient('mediumVioletRed', 'fuchsia'),border='pink',borderWidth=1)
+        drawLabel(f'{app.score}',625,26,font='caveat',fill='lemonChiffon',
+                  size=22,border='lemonChiffon',borderWidth=1,align='center')
+        drawTrail(app)
+        drawCircle(app.handX, app.handY, 8, fill=gradient('pink','mediumVioletRed', 'fuchsia'),
+                   border='pink',borderWidth=2)
     drawButtons(app)
 
 def main():
